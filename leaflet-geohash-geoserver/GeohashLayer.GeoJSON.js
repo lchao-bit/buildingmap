0723e80f4149b4b16b4025e1e4798a4bc072758d
zchang <imivyzhou@hotmail.com>
2018-07-10 22:56:57 +0800

    地图客户端可以展示geohash地图

---
+++ leaflet-geohash-geoserver/GeohashLayer.GeoJSON.js
@@ -0,0 +1,330 @@
+//zc,Load Geohash map from blockchain
+// Load data tiles from a blockchain data source
+L.GeohashLayer.blockchain = L.GeohashLayer.extend({
+    _requests: [],
+    _addTile: function (tilePoint) {
+    		 var tile = { datum: null, processed: false };	 			
+	    		 this._tiles[tilePoint] = tile;
+	         this._loadTile(tile, tilePoint);
+    },
+    // zc,Load the requested geohash data of blockchain
+    /*_loadTile: function (tile, geohashPoint) {
+        //this._adjustTilePoint(tilePoint);
+        var layer = this;
+        
+				//not in buffer, get roads from block chain
+					var roads_array = new Array();
+					myContractInstance.get_roads(geohashPoint).then(function(roads){
+			  	var road_info = roads[0];
+					var road_name = roads[1];
+					var road_path = roads[2];
+					var pos = 0;//record pointer to read path
+			
+					for(var i=0; i<road_name.length; i++){
+						var road = new Object();
+						road.gid = road_info[i*8].valueOf();
+						
+						road.x1  = road_info[i*8 + 1] / 100000;
+						road.y1  = road_info[i*8 + 2] / 100000;
+						road.x2  = road_info[i*8 + 3] / 100000;
+						road.y2  = road_info[i*8 + 4] / 100000;
+						road.source = road_info[i*8 + 5].valueOf();
+						road.target = road_info[i*8 + 6].valueOf();
+						road.oneway  = road_info[i*8 + 7].valueOf();
+						road.name = hex2str(road_name[i]);
+						
+						road.path = new Array();
+						road.path.push(road.x1);
+						road.path.push(road.y1);
+			
+						//read path
+						var path_num = road_path[pos++];
+						for(var j=0; j< path_num; j++){
+							road.path.push(road_path[pos++] / 100000);
+						}
+						road.path.push(road.x2);
+						road.path.push(road.y2);
+			
+						roads_array.push(road);
+					}
+					return roads_array;
+				}).then(function(roads_array){
+					// define geojson obj from data
+				  var geohashdata = {
+				    type: 'FeatureCollection',
+				    features: []
+				  };
+				  var coords,co,curpath;
+				  var curroads  = roads_array;
+				  for(var i = 0;i < curroads.length;i++){
+				  	coords = [];
+				  	co = [];
+				  	curpath = curroads[i].path;
+				  	for(var j = 0; j< curpath.length-1;j = j+2){
+				  		coords.push([curpath[j],curpath[j+1]]);
+				  	}
+				  	// push to geojson
+				    geohashdata.features.push({
+				      type: 'Feature',
+				      properties: {
+				      },
+				      geometry: {
+				        type: 'LineString',
+				        coordinates: coords
+				      }
+				    });
+	        }
+	        //tile.datum = JSON.parse(geohashdata);
+					
+				    tile.datum = geohashdata;
+					
+	          layer._tileLoaded(tile, geohashPoint);
+				});			
+    },*/
+    // XMLHttpRequest handler; closure over the XHR object, the layer, and the tile
+    _xhrHandler: function (req, layer, tile, tilePoint) {
+        return function () {
+            if (req.readyState !== 4) {
+                return;
+            }
+            var s = req.status;
+            if ((s >= 200 && s < 300 && s != 204) || s === 304) {
+                tile.datum = JSON.parse(req.responseText);
+                layer._tileLoaded(tile, tilePoint);
+            } else {
+                layer._tileLoaded(tile, tilePoint);
+            }
+        };
+    },
+    // Load the requested geohash map via AJAX
+    _loadTile: function (tile, tilePoint) {
+        //this._adjustTilePoint(tilePoint);
+        var layer = this;
+        var req = new XMLHttpRequest();
+        this._requests.push(req);
+        req.onreadystatechange = this._xhrHandler(req, layer, tile, tilePoint);
+        req.open('GET', this.getTileUrl(tilePoint), true);
+        req.send();
+    },
+    _reset: function () {
+        L.GeohashLayer.prototype._reset.apply(this, arguments);
+        for (var i = 0; i < this._requests.length; i++) {
+            this._requests[i].abort();
+        }
+        this._requests = [];
+    },
+    _update: function () {
+        if (this._map && this._map._panTransition && this._map._panTransition._inProgress) { return; }
+        if (this._tilesToLoad < 0) { this._tilesToLoad = 0; }
+        L.GeohashLayer.prototype._update.apply(this, arguments);
+    }
+});
+
+L.GeohashLayer.GeoJSON = L.GeohashLayer.blockchain.extend({
+	// Store each GeometryCollection's layer by key, if options.unique function is present
+    _keyLayers: {},
+
+    // Used to calculate svg path string for clip path elements
+    _clipPathRectangles: {},
+
+    initialize: function (url, options, geojsonOptions) {
+        L.GeohashLayer.blockchain.prototype.initialize.call(this, url, options);
+        this.geojsonLayer = new L.GeoJSON(null, geojsonOptions);
+    },
+    onAdd: function (map) {
+        this._map = map;
+        L.GeohashLayer.blockchain.prototype.onAdd.call(this, map);
+        map.addLayer(this.geojsonLayer);
+    },
+    onRemove: function (map) {
+        map.removeLayer(this.geojsonLayer);
+        L.GeohashLayer.blockchain.prototype.onRemove.call(this, map);
+    },
+    _reset: function () {
+        this.geojsonLayer.clearLayers();
+        this._keyLayers = {};
+        this._removeOldClipPaths();
+        L.GeohashLayer.blockchain.prototype._reset.apply(this, arguments);
+    },
+
+    // Remove clip path elements from other earlier zoom levels
+    _removeOldClipPaths: function  () {
+        for (var clipPathId in this._clipPathRectangles) {
+            var clipPathZXY = clipPathId.split('_').slice(1);
+            var zoom = parseInt(clipPathZXY[0], 10);
+            if (zoom !== this._map.getZoom()) {
+                var rectangle = this._clipPathRectangles[clipPathId];
+                this._map.removeLayer(rectangle);
+                var clipPath = document.getElementById(clipPathId);
+                if (clipPath !== null) {
+                    clipPath.parentNode.removeChild(clipPath);
+                }
+                delete this._clipPathRectangles[clipPathId];
+            }
+        }
+    },
+
+    // Recurse LayerGroups and call func() on L.Path layer instances
+    _recurseLayerUntilPath: function (func, layer) {
+        if (layer instanceof L.Path) {
+            func(layer);
+        }
+        else if (layer instanceof L.LayerGroup) {
+            // Recurse each child layer
+            layer.getLayers().forEach(this._recurseLayerUntilPath.bind(this, func), this);
+        }
+    },
+
+    _clipLayerToTileBoundary: function (layer, tilePoint) {
+        // Only perform SVG clipping if the browser is using SVG
+        if (!L.Path.SVG) { return; }
+        if (!this._map) { return; }
+
+        if (!this._map._pathRoot) {
+            this._map._pathRoot = L.Path.prototype._createElement('svg');
+            this._map._panes.overlayPane.appendChild(this._map._pathRoot);
+        }
+        var svg = this._map._pathRoot;
+
+        // create the defs container if it doesn't exist
+        var defs = null;
+        if (svg.getElementsByTagName('defs').length === 0) {
+            defs = document.createElementNS(L.Path.SVG_NS, 'defs');
+            svg.insertBefore(defs, svg.firstChild);
+        }
+        else {
+            defs = svg.getElementsByTagName('defs')[0];
+        }
+
+        // Create the clipPath for the tile if it doesn't exist
+        var clipPathId = 'tileClipPath_' + tilePoint.z + '_' + tilePoint.x + '_' + tilePoint.y;
+        var clipPath = document.getElementById(clipPathId);
+        if (clipPath === null) {
+            clipPath = document.createElementNS(L.Path.SVG_NS, 'clipPath');
+            clipPath.id = clipPathId;
+
+            // Create a hidden L.Rectangle to represent the tile's area
+            var tileSize = this.options.tileSize,
+            nwPoint = tilePoint.multiplyBy(tileSize),
+            sePoint = nwPoint.add([tileSize, tileSize]),
+            nw = this._map.unproject(nwPoint),
+            se = this._map.unproject(sePoint);
+            this._clipPathRectangles[clipPathId] = new L.Rectangle(new L.LatLngBounds([nw, se]), {
+                opacity: 0,
+                fillOpacity: 0,
+                clickable: false,
+                noClip: true
+            });
+            this._map.addLayer(this._clipPathRectangles[clipPathId]);
+
+            // Add a clip path element to the SVG defs element
+            // With a path element that has the hidden rectangle's SVG path string  
+            var path = document.createElementNS(L.Path.SVG_NS, 'path');
+            var pathString = this._clipPathRectangles[clipPathId].getPathString();
+            path.setAttribute('d', pathString);
+            clipPath.appendChild(path);
+            defs.appendChild(clipPath);
+        }
+
+        // Add the clip-path attribute to reference the id of the tile clipPath
+        this._recurseLayerUntilPath(function (pathLayer) {
+            pathLayer._container.setAttribute('clip-path', 'url(#' + clipPathId + ')');
+        }, layer);
+    },
+
+    // Add a geojson object from a tile to the GeoJSON layer
+    // * If the options.unique function is specified, merge geometries into GeometryCollections
+    // grouped by the key returned by options.unique(feature) for each GeoJSON feature
+    // * If options.clipTiles is set, and the browser is using SVG, perform SVG clipping on each
+    // tile's GeometryCollection 
+    addTileData: function (geojson, tilePoint) {
+        var features = L.Util.isArray(geojson) ? geojson : geojson.features,
+            i, len, feature;
+
+        if (features) {
+            for (i = 0, len = features.length; i < len; i++) {
+                // Only add this if geometry or geometries are set and not null
+                feature = features[i];
+                if (feature.geometries || feature.geometry || feature.features || feature.coordinates) {
+                    this.addTileData(features[i], tilePoint);
+                }
+            }
+            return this;
+        }
+
+        var options = this.geojsonLayer.options;
+
+        if (options.filter && !options.filter(geojson)) { return; }
+
+        var parentLayer = this.geojsonLayer;
+        var incomingLayer = null;
+        if (this.options.unique && typeof(this.options.unique) === 'function') {
+            var key = this.options.unique(geojson);
+
+            // When creating the layer for a unique key,
+            // Force the geojson to be a geometry collection
+            if (!(key in this._keyLayers && geojson.geometry.type !== 'GeometryCollection')) {
+                geojson.geometry = {
+                    type: 'GeometryCollection',
+                    geometries: [geojson.geometry]
+                };
+            }
+
+            // Transform the geojson into a new Layer
+            try {
+                incomingLayer = L.GeoJSON.geometryToLayer(geojson, options.pointToLayer, options.coordsToLatLng);
+            }
+            // Ignore GeoJSON objects that could not be parsed
+            catch (e) {
+                return this;
+            }
+
+            incomingLayer.feature = L.GeoJSON.asFeature(geojson);
+            // Add the incoming Layer to existing key's GeometryCollection
+            if (key in this._keyLayers) {
+                parentLayer = this._keyLayers[key];
+                parentLayer.feature.geometry.geometries.push(geojson.geometry);
+            }
+            // Convert the incoming GeoJSON feature into a new GeometryCollection layer
+            else {
+                this._keyLayers[key] = incomingLayer;
+            }
+        }
+        // Add the incoming geojson feature to the L.GeoJSON Layer
+        else {
+            // Transform the geojson into a new layer
+            try {
+                incomingLayer = L.GeoJSON.geometryToLayer(geojson, options.pointToLayer, options.coordsToLatLng);
+            }
+            // Ignore GeoJSON objects that could not be parsed
+            catch (e) {
+                return this;
+            }
+            incomingLayer.feature = L.GeoJSON.asFeature(geojson);
+        }
+        incomingLayer.defaultOptions = incomingLayer.options;
+
+        this.geojsonLayer.resetStyle(incomingLayer);
+
+        if (options.onEachFeature) {
+            options.onEachFeature(geojson, incomingLayer);
+        }
+        parentLayer.addLayer(incomingLayer);
+
+        // If options.clipTiles is set and the browser is using SVG
+        // then clip the layer using SVG clipping
+        if (this.options.clipTiles) {
+            this._clipLayerToTileBoundary(incomingLayer, tilePoint);
+        }
+        return this;
+    },
+
+    _tileLoaded: function (tile, tilePoint) {
+        L.GeohashLayer.blockchain.prototype._tileLoaded.apply(this, arguments);
+        if (tile.datum === null) { 
+        	alert(tilePoint);
+        	return null; }
+        this.addTileData(tile.datum, tilePoint);
+    }
+}); 
+ 
